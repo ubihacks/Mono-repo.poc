eedcd9c7b1f9adecc5ceac18a498dd7c206141a6
ubihacks <ubaidifti@gmail.com>
2022-11-17 18:31:23 +0500

    ad tessst

--- apps/admin-portal/src/App.tsx
+++ apps/admin-portal/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     
       <Box maxW='sm' borderWidth='1px' borderRadius='lg' >
       <Heading>Admin Portal</Heading>
-      <Text>Turbo POC wiht Chakra Ui Admin test </Text>
+      <Text>Turbo POC wiht Chakra Ui Admin  </Text>
       </Box>
     
 
